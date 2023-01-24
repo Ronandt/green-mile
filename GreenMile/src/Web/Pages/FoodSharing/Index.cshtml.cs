c66167377e2822deed98b35bc30dbe14c522a939
HProsperouS <96221600+HProsperouS@users.noreply.github.com>
2023-01-24 23:26:07 +0800

    SendEmail

--- GreenMile/src/Web/Pages/FoodSharing/Index.cshtml.cs
+++ GreenMile/src/Web/Pages/FoodSharing/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Web.Models;
 using Web.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Net.Mail;
 
 namespace Web.Pages.FoodSharing
 {
@@ -55,6 +57,23 @@ namespace Web.Pages.FoodSharing
                 
             if (exists == false)
             {
+
+                MailMessage message = new MailMessage();
+                message.To.Add("liujiajun2003@gmail.com");
+                message.Subject = "Test Email";
+                message.Body = "This is a test email sent from a Razor Page.";
+                message.IsBodyHtml = false;
+                message.From = new MailAddress("GreenMileByCrazyAPI@gmail.com");
+
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.UseDefaultCredentials = false;
+                client.EnableSsl = true;
+                // Green Mile Email Account Password: greenmile2023!
+                client.Credentials = new NetworkCredential("GreenMileByCrazyAPI@gmail.com", "arcpfpypntqmhnxf");
+                await client.SendMailAsync(message);
+
                 var request = new DonationRequest()
                 {
                     Donation = donation,
